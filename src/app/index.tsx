5ce1da4c8ce7531ae8d23f238684ef12dd063b6c
Ra1ymkulov <retype.uun@gmail.com>
2026-04-03 15:52:48 +0600

    dev

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,16 +1,15 @@
-import React from 'react';
+import React from "react";
 import "@/shared/assets/styles/vars.css";
-import ReactDOM from 'react-dom/client';
-import './index.scss';
-import '../locales/i18n';
+import ReactDOM from "react-dom/client";
+import "./index.scss";
+import "../locales/i18n";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-w
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement,
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
+  </BrowserRouter>
 );
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,14 +1,14 @@
 import { Banners } from "@/widgets/Banners";
-import { Movement } from "@/widgets/Movment";
 import BrandsPages from "./BrandsPages/BrandsPages";
 import { EventsPages } from "./EventsPages/EventsPages";
 import { NewsPages } from "./NewsPages/NewsPages";
+import { MovementPages } from "./Movement/ui/MovementPages";
 
 const MainPage = () => {
   return (
     <div>
       <Banners />
-      <Movement />
+      <MovementPages />
       <EventsPages />
       <NewsPages />
       <BrandsPages />
