a8ff51c4a2d258e81f9c43995173eb19d19f08d5
avazjanovsardorbek01 <sardoravazzanov@gmail.com>
2026-04-03 14:59:28 +0500

    Add-Media

--- src/pages/Media/ui/PhotoGallery/PhotoGallery.tsx
+++ src/pages/Media/ui/PhotoGallery/PhotoGallery.tsx
@@ -1,75 +1,82 @@
-import { ArrowRightIcon } from 'lucide-react';
-import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { PhotoCard } from '../PhotoCard/PhotoCard';
-import styles from './PhotoGallery.module.scss';
-import { useTranslation } from 'react-i18next';
-import { useImagesStore } from '@/app/store/Media/images';
+import { ArrowRightIcon } from "lucide-react";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { PhotoCard } from "../PhotoCard/PhotoCard";
+import styles from "./PhotoGallery.module.scss";
+import { useTranslation } from "react-i18next";
 
-// Определение интерфейсов
-interface GalleryImage {
-  id: number;
-  gallery: number;
-  image: string;
-}
 
-interface ApiImageItem {
-  id: number;
-  title: string;
-  date?: string;
-  images: GalleryImage[];
-}
-
-interface ImageItem {
-  id: number;
-  title: string;
-  date: string;
-  imageUrl: string;
-}
+const mockImages = [
+  {
+    id: 1,
+    title: "Название фото “Название”",
+    date: "12.03.2025",
+    image: "https://images.unsplash.com/photo-1502082553048-f009c37129b9",
+  },
+  {
+    id: 2,
+    title: "Название фото “Название”",
+    date: "12.03.2025",
+    image: "https://images.unsplash.com/photo-1529156069898-49953e39b3ac",
+  },
+  {
+    id: 3,
+    title: "Название фото “Название”",
+    date: "12.03.2025",
+    image: "https://images.unsplash.com/photo-1500530855697-b586d89ba3ee",
+  },
+  {
+    id: 4,
+    title: "Название фото “Название”",
+    date: "12.03.2025",
+    image: "https://images.unsplash.com/photo-1470770841072-f978cf4d019e",
+  },
+  {
+    id: 5,
+    title: "Название фото “Название”",
+    date: "12.03.2025",
+    image: "https://images.unsplash.com/photo-1507525428034-b723cf961d3e",
+  },
+  {
+    id: 6,
+    title: "Название фото “Название”",
+    date: "12.03.2025",
+    image: "https://images.unsplash.com/photo-1492724441997-5dc865305da7",
+  },
+];
 
 export const PhotoGallery: React.FC = () => {
-  const { imagesCards, loading, error, fetchImages } = useImagesStore();
-
-  useEffect(() => {
-    fetchImages();
-  }, [fetchImages]);
-
-
   const navigate = useNavigate();
-  const { t, i18n } = useTranslation()
-  console.log("Current language:", imagesCards);
-  
-    // const navigate = useNavigate();
-    // const {t , i18n} = useTranslation()
+  const { t } = useTranslation();
+
   return (
     <div className={styles.container}>
       <header className={styles.header}>
-        <h1 className={styles.title}>{t('media.PhotoGallery')}</h1>
-        
-        <button onClick={() => navigate("/photoGallery")} className={styles.button}>
-          <span className={styles.buttonText}>{t('media.allPhoto')}</span>
+        <h1 className={styles.title}>
+          {t("media.PhotoGallery") || "Фотогалерея"}
+        </h1>
+
+        <button
+          onClick={() => navigate("/photoGallery")}
+          className={styles.button}
+        >
+          <span className={styles.buttonText}>
+            {t("media.allPhoto") || "Все фото"}
+          </span>
           <ArrowRightIcon className={styles.buttonIcon} />
         </button>
       </header>
 
       <main className={styles.gallery}>
-        {loading ? (
-          <p>Загрузка...</p>
-        ) : error ? (
-          <p className={styles.error}>Ошибка: {error}</p>
-        ) : imagesCards.length === 0 ? (
-          <p>Нет доступных изображений.</p>
-        ) : (
-          imagesCards.slice(0, 6).map((item) => (
-            <PhotoCard
-              key={item.id}
-              id={item.id}
-              date={item.date}
-              title={item.title}
-              imageUrl={item.image}
-            />
-          ))
-        )}
+        {mockImages.slice(0, 6).map((item) => (
+          <PhotoCard
+            key={item.id}
+            id={item.id}
+            date={item.date}
+            title={item.title}
+            imageUrl={item.image}
+          />
+        ))}
       </main>
     </div>
   );
--- src/pages/Photo/PhotoGallry.tsx
+++ src/pages/Photo/PhotoGallry.tsx
@@ -5,99 +5,104 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styles from "./PhotoGallry.module.scss";
 import { useTranslation } from "react-i18next";
-import { IoIosArrowForward, IoIosArrowBack  } from "react-icons/io";
 import { useImagesStore } from "@/app/store/Media/images";
 
-
 const albums = Array.from({ length: 42 }, (_, i) => ({
-    id: i + 1,
-    title: `Альбом ${i + 1}`,
-    event: `Событие ${Math.ceil((i + 1) / 3)}`,
-    imageUrl: `https://picsum.photos/seed/${i + 1}/400/300`,
-    count: Math.floor(Math.random() * 50) + 1,
+  id: i + 1,
+  title: `Альбом ${i + 1}`,
+  event: `Событие ${Math.ceil((i + 1) / 3)}`,
+  imageUrl: `https://picsum.photos/seed/${i + 1}/400/300`,
+  count: Math.floor(Math.random() * 50) + 1,
 }));
 
 export function PhotoGallry() {
-    const [currentPage, setCurrentPage] = useState(1);
-    const itemsPerPage = 9;
-    const navigate = useNavigate();
-    const totalPages = Math.ceil(albums.length / itemsPerPage);
-    const startIndex = (currentPage - 1) * itemsPerPage;
-    const currentAlbums = albums.slice(startIndex, startIndex + itemsPerPage);
-    const {t , i18n} = useTranslation()
-    const {loading, error, imagesCards, fetchImages} = useImagesStore()
-        if (loading) {
-        return <div className="loader"></div>;
-    }
-    if (error) {
-        return <p style={{ color: "red" }}>{error}</p>;
-    }
-    return (
-        <div className={styles.container}>
-            {/* Навигация */}
-            <div className={styles.breadcrumbs}>
-              <Navpanel text={t('PhotoGallery.home')} text2={t('PhotoGallery.media')} text3={t('PhotoGallery.PhotoGallery')} link="/" link2="/media"/>
-            </div>
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 9;
+  const navigate = useNavigate();
+  const totalPages = Math.ceil(albums.length / itemsPerPage);
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const currentAlbums = albums.slice(startIndex, startIndex + itemsPerPage);
+  const { t, i18n } = useTranslation();
+  const { loading, error, imagesCards, fetchImages } = useImagesStore();
+  if (loading) {
+    return <div className="loader"></div>;
+  }
+  if (error) {
+    return <p style={{ color: "red" }}>{error}</p>;
+  }
+  return (
+    <div className={styles.container}>
+      {/* Навигация */}
+      <div className={styles.breadcrumbs}>
+        <Navpanel
+          text={t("PhotoGallery.home")}
+          text2={t("PhotoGallery.media")}
+          text3={t("PhotoGallery.PhotoGallery")}
+          link="/"
+          link2="/media"
+        />
+      </div>
 
-            {/* Заголовок */}
-            <header className={styles.header}>
-                <h1 className={styles.title}>{t('PhotoGallery.PhotoGallery')}</h1>
-                <div className={styles.buttons}>
-                    <button className={styles.button} aria-label="Посмотреть все фото">
-                        <span className={styles.buttonText}>{t('PhotoGallery.selectDate')}</span>
-                        <ArrowRightIcon className={styles.buttonIcon} />
-                    </button>
-                    <button className={styles.button} onClick={() => navigate("/media")}>
-                        <span className={styles.buttonText}>{t('PhotoGallery.goBack')}</span>
-                        <ArrowRightIcon className={styles.buttonIcon} />
-                    </button>
-                </div>
-            </header>
-
-            {/* Галерея */}
-            <div className={styles.gallery}>
-                {currentAlbums.map((album) => (
-                    <AlbumCard
-                        key={album.id}
-                        title={album.title}
-                        event={album.event}
-                        imageUrl={album.imageUrl}
-                        count={album.count}
-                    />
-                ))}
-            </div>
-
-            {/* Пагинация */}
-            <div className={styles.pagination}>
-                <button
-                    className={styles.pageButton}
-                    onClick={() => setCurrentPage((p) => Math.max(p - 1, 1))}
-                    disabled={currentPage === 1}
-                >
-                    <IoIosArrowBack/>
-                </button>
-
-                <div className={styles.pageNumbers}>
-                    {Array.from({ length: totalPages }, (_, i) => (
-                        <button
-                            key={i + 1}
-                            className={`${styles.pageNumber} ${currentPage === i + 1 ? styles.active : ""
-                                }`}
-                            onClick={() => setCurrentPage(i + 1)}
-                        >
-                            {i + 1}
-                        </button>
-                    ))}
-                </div>
-
-                <button
-                    className={styles.pageButton}
-                    onClick={() => setCurrentPage((p) => Math.min(p + 1, totalPages))}
-                    disabled={currentPage === totalPages}
-                >
-                    <IoIosArrowForward />
-                </button>
-            </div>
+      {/* Заголовок */}
+      <header className={styles.header}>
+        <h1 className={styles.title}>{t("PhotoGallery.PhotoGallery")}</h1>
+        <div className={styles.buttons}>
+          <button className={styles.button} aria-label="Посмотреть все фото">
+            <span className={styles.buttonText}>
+              {t("PhotoGallery.selectDate")}
+            </span>
+            <ArrowRightIcon className={styles.buttonIcon} />
+          </button>
+          <button className={styles.button} onClick={() => navigate("/media")}>
+            <span className={styles.buttonText}>
+              {t("PhotoGallery.goBack")}
+            </span>
+            <ArrowRightIcon className={styles.buttonIcon} />
+          </button>
         </div>
-    );
+      </header>
+
+      {/* Галерея */}
+      <div className={styles.gallery}>
+        {currentAlbums.map((album) => (
+          <AlbumCard
+            key={album.id}
+            title={album.title}
+            event={album.event}
+            imageUrl={album.imageUrl}
+            count={album.count}
+          />
+        ))}
+      </div>
+
+      {/* Пагинация */}
+      <div className={styles.pagination}>
+        <button
+          className={styles.pageButton}
+          onClick={() => setCurrentPage((p) => Math.max(p - 1, 1))}
+          disabled={currentPage === 1}
+        ></button>
+
+        <div className={styles.pageNumbers}>
+          {Array.from({ length: totalPages }, (_, i) => (
+            <button
+              key={i + 1}
+              className={`${styles.pageNumber} ${
+                currentPage === i + 1 ? styles.active : ""
+              }`}
+              onClick={() => setCurrentPage(i + 1)}
+            >
+              {i + 1}
+            </button>
+          ))}
+        </div>
+
+        <button
+          className={styles.pageButton}
+          onClick={() => setCurrentPage((p) => Math.min(p + 1, totalPages))}
+          disabled={currentPage === totalPages}
+        ></button>
+      </div>
+    </div>
+  );
 }
