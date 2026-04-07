dd7d764b5ab5c2f714f61576fa68f09b1f3d1920
avazjanovsardorbek01 <sardoravazzanov@gmail.com>
2026-04-07 15:41:27 +0500

    Profile Update

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,11 +2,15 @@ import "./App.scss";
 import { Footer } from "../widgets/Footer/ui/Footer";
 import { Routing } from "./router";
 import Header from "@/widgets/Header/Header";
+import { useLocation } from "react-router-dom";
 
 function App() {
+  const location = useLocation();
+  const isProfilePage = location.pathname === "/profile";
+
   return (
     <div>
-      <Header />
+      {!isProfilePage && <Header />}
       <main className="routing">
         <Routing />
       </main>
